63a049fc948fcafba8fefd4dbcccf96c09b2207e
Camille <wilhelm.cam@gmail.com>
2019-05-01 16:54:25 -0400

    Added all files in Acado with and without muscles

---
+++ optimal_control/SansMuscle.cpp
@@ -0,0 +1,134 @@
+#include <acado_optimal_control.hpp>
+#include <bindings/acado_gnuplot/gnuplot_window.hpp>
+#include <s2mMusculoSkeletalModel.h>
+
+using namespace std;
+USING_NAMESPACE_ACADO
+
+/* ---------- Model ---------- */
+
+s2mMusculoSkeletalModel m("../Modele/ModeleSansMuscle.s2mMod");
+
+
+static int nQ(static_cast<int>(m.nbQ()));               // states number
+static int nQdot(static_cast<int>(m.nbQdot()));         // derived states number
+static int nTau(static_cast<int>(m.nbTau()));           // controls number
+static int nTags(static_cast<int>(m.nTags()));          // markers number
+
+
+const double t_Start=0.0;
+const double t_End= 10.0;
+const int nPoints(30);
+
+
+/* ---------- Functions ---------- */
+
+#define  NX   nQ + nQdot        // number of differential states
+void fowardDynamics( double *x, double *rhs, void *user_data){
+    s2mGenCoord Q(static_cast<unsigned int>(nQ));           // states
+    s2mGenCoord Qdot(static_cast<unsigned int>(nQdot));     // derivated states
+    s2mTau Tau(static_cast<unsigned int>(nTau));            // controls
+    s2mGenCoord Qddot(static_cast<unsigned int>(nQdot));
+
+    for (int i = 0; i<nQ; ++i){
+        Q[i] = x[i];
+        Qdot[i] = x[i+nQ];
+        Tau[i] = x[i+nQ+nQdot];
+    }
+
+    RigidBodyDynamics::ForwardDynamics(m, Q, Qdot, Tau, Qddot);
+
+    for (int i = 0; i<nQ; ++i){
+       rhs[i] = Qdot[i];
+       rhs[i + nQdot] = Qddot[i];
+   }
+}
+
+#define  NOL   1                 // number of lagrange objective functions
+void myLagrangeObjectiveFunction( double *x, double *g, void *user_data ){
+    g[0] = 0;
+}
+
+#define  NOM   1                 // number of mayer objective functions
+void myMayerObjectiveFunction( double *x, double *g, void *user_data ){
+    double obj = x[0]-PI/2;
+    g[0] = obj*obj;
+}
+
+#define  NI   2                 // number of initial value constraints
+void myInitialValueConstraint( double *x, double *g, void *user_data ){
+    for (int i =0; i<nQ + nQdot; ++i) {
+        g[i] =  x[i];
+    }
+}
+
+#define  NE   2                 // number of end-point / terminal constraints
+void myEndPointConstraint( double *x, double *g, void *user_data ){
+    g[0]=x[0]-PI/2;                         // rotation de 90°
+    g[0]=x[1];                              // vitesse nulle
+}
+
+
+int  main ()
+{
+    /* ---------- INITIALIZATION ---------- */
+    Parameter               T;                              //  the  time  horizon T
+    DifferentialState       x("",nQ+nQdot,1);               //  the  differential states
+    Control                 u("", nTau, 1);          //  the  control input  u
+    IntermediateState is(nQ + nQdot + nTau);
+
+    for (int i = 0; i < nQ; ++i)
+        is(i) = x(i);                   //*scalingQ(i);
+    for (int i = 0; i < nQdot; ++i)
+        is(i+nQ) = x(i+nQ);             //*scalingQdot(i);
+    for (int i = 0; i < nTau; ++i)
+        is(i+nQ+nQdot) = u(i);          //*scalingQdot(i);
+
+    /* ----------- DEFINE OCP ------------- */
+    CFunction Mayer( NOM, myMayerObjectiveFunction);
+    CFunction Lagrange( NOL, myLagrangeObjectiveFunction);
+    OCP ocp( t_Start, T , nPoints);                        // time  horizon
+    ocp.minimizeMayerTerm( Mayer(is) );                    // Mayer term
+    ocp.minimizeLagrangeTerm( Lagrange(is) );                    // Lagrange term
+
+
+    /* ------------ CONSTRAINTS ----------- */
+    DifferentialEquation    f;                             //  the  differential  equation
+    CFunction F( NX, fowardDynamics);
+    CFunction I( NI, myInitialValueConstraint   );
+    CFunction E( 1, myEndPointConstraint       );
+
+    ocp.subjectTo( (f << dot(x)) == F(is) );                          //  differential  equation,
+    ocp.subjectTo( AT_START, I(is) ==  0.0 );
+    ocp.subjectTo( AT_END  , E(is) ==  0.0 );
+    ocp.subjectTo(-100 <= u <= 100);
+
+
+
+    /* ---------- OPTIMIZATION  ------------ */
+    OptimizationAlgorithm  algorithm( ocp ) ;       //  construct optimization  algorithm ,
+
+    VariablesGrid u_init(1, Grid(t_Start, t_End, 2));
+    u_init(0, 0) = 0.1;
+    u_init(0, 1) = 0.1;
+    algorithm.initializeControls(u_init);
+
+    VariablesGrid x_init(2, Grid(t_Start, t_End, 2));
+    x_init(0, 0) = 0.1;
+    x_init(0, 1) = 0.1;
+    x_init(1, 0) = 0.1;
+    x_init(1, 1) = 0.1;
+    algorithm.initializeDifferentialStates(x_init);
+
+    GnuplotWindow window;                           //  visualize  the  results  in  a  Gnuplot  window
+    window.addSubplot(  x ,  "DISTANCE x" ) ;
+    window.addSubplot( u ,  "CONTROL  u" ) ;
+    algorithm << window;
+    algorithm.solve();                              //  and solve the problem .
+
+    algorithm.getDifferentialStates("../Resultats/StatesSansMuscle.txt");
+    algorithm.getControls("../Resultats/ControlsSansMuscle.txt");
+
+    return 0;
+}
+
---
+++ optimal_control/show_model.py
@@ -0,0 +1,186 @@
+"""
+Example script for animating a model
+"""
+
+from pathlib import Path
+import copy
+
+import numpy as np
+import scipy.integrate as integrate
+import scipy.interpolate as interpolate
+
+from pyomeca import Markers3d, RotoTrans, RotoTransCollection
+from pyoviz.vtk import VtkModel, VtkWindow, Mesh, MeshCollection
+import biorbd
+
+import matplotlib.pyplot as plt
+
+# Load the biorbd model
+nFrame = 500            # number of frames
+nNoeuds = 30                # number of points
+nMuscle = 1
+nPhase = 2
+nPoints = (nPhase*nNoeuds)+1
+
+m = biorbd.s2mMusculoSkeletalModel("/home/laboratoire/mnt/E/Bureau/Camille/Acado_sandbox/Activation1Muscle/ModeleEx1AvMuscle.s2mMod")
+
+
+##### State from the file
+
+fichierS = open("/home/laboratoire/mnt/E/Bureau/Camille/Acado_sandbox/debug/statesAvMuscle.txt", "r")
+
+i = 0
+t = []                                      # initialization of the time
+nQtotal = m.nbQ()+m.nbQdot()
+
+all_Q = np.ones((m.nbQ(), nPoints))             # initialization of the states nbQ lines and nbP columns
+all_Qdot = np.ones((m.nbQdot(), nPoints))
+
+# initialization of the derived states
+
+# Nnoeuds first lines
+for l in range(nNoeuds):
+    ligne = fichierS.readline()
+    lin = ligne.split('\t')                 # separation of the line in element
+    lin[:1] = []                            # remove the first element ( [ )
+    lin[(nPhase*m.nbQ()) + (nPhase*m.nbQdot()) + 1:] = []     # remove the last ( ] )
+
+    t.append(float(lin[0]))                                                         # complete the time with the first column
+    for p in range(nPhase):
+        all_Q[:, i+p*nNoeuds] = [float(j) for j in lin[1+p*nQtotal:m.nbQ()+p*nQtotal+1]]                              # complete the states with the nQ next columns
+        all_Qdot[:, i+p*nNoeuds] = [float(k) for k in lin[m.nbQ()+1+p*nQtotal:nQtotal*(p+1)+1]]        # complete the states with the nQdot next columns
+
+    i += 1
+# Last line
+ligne = fichierS.readline()
+lin = ligne.split('\t')                 # separation of the line in element
+lin[:1] = []                            # remove the first element ( [ )
+lin[(nPhase*m.nbQ()) + (nPhase*m.nbQdot()) + 1:] = []     # remove the last ( ] )
+t.append(float(lin[0]))
+all_Q[:, -1] = [float(j) for j in lin[1+(nPhase-1)*nQtotal:m.nbQ()+(nPhase-1)*nQtotal+1]]                              # complete the states with the nQ next columns
+all_Qdot[:, -1] = [float(k) for k in lin[m.nbQ()+1+(nPhase-1)*nQtotal:nQtotal*nPhase+1]]
+
+
+fichierS.close()
+
+# Complete the time
+for p in range(1, nPhase):
+    for i in range(nNoeuds):
+        t.append(p+t[i+1])
+
+##### Controls from the file
+
+fichierU = open("/home/laboratoire/mnt/E/Bureau/Camille/Acado_sandbox/debug/controlsAvMuscle.txt", "r")
+
+i = 0
+all_U = np.ones((m.nbQdot(), nPoints))          # initialization of the controls
+
+for l in range(nNoeuds):
+    ligne = fichierU.readline()
+    lin = ligne.split('\t')
+    lin[:1] = []
+    lin[(2*nMuscle) + 1:] = []
+
+    for p in range(nPhase):
+        all_U[:, i+p*nNoeuds] = [float(i) for i in lin[1+p*nMuscle:nMuscle*(p+1)+1]]
+
+    i += 1
+
+ligne = fichierU.readline()
+lin = ligne.split('\t')
+lin[:1] = []
+lin[(2*nMuscle) + 1:] = []
+
+all_U[:, -1] = [float(i) for i in lin[1+(nPhase-1)*nMuscle:nMuscle*nPhase+1]]
+
+fichierU.close()
+
+#### integration
+
+def dyn(t_int, X):
+    states_actual = biorbd.VecS2mMuscleStateActual(nMuscle)
+    for i in range(len(states_actual)):
+        states_actual[i] = biorbd.s2mMuscleStateActual(0, u[i])
+
+    Tau = biorbd.s2mMusculoSkeletalModel.muscularJointTorque(m, states_actual, X[:m.nbQ()], X[m.nbQ():])
+
+    QDDot = biorbd.s2mMusculoSkeletalModel.ForwardDynamics(m, X[:m.nbQ()], X[m.nbQ():], Tau).get_array()
+    rsh = np.ndarray(m.nbQ() + m.nbQdot())
+    for i in range(m.nbQ()):
+        rsh[i] = X[m.nbQ()+i]
+        rsh[i + m.nbQ()] = QDDot[i]
+
+    return rsh
+
+
+Y = list()                                  # interpolated states list
+for q in range(m.nbQ()):
+    T = []                                # time list
+    I = []
+    X = np.concatenate((all_Q[:, 0], all_Qdot[:, 0]))                                    # integrated states list
+    for interval in range(len(t)-1):                                                    # integration between each point
+        u = all_U[:, interval]
+        LI = integrate.solve_ivp(dyn, (t[interval], t[interval+1]), X)
+        X = LI.y[:, -1]
+
+        for i in range(len(LI.t)-1):
+            I.append(LI.y[q, :].tolist()[i])
+            T.append(LI.t.tolist()[i])
+
+    #### Interpolation
+
+    tck = interpolate.splrep(T, I, s=0)
+    time = np.linspace(0, T[len(T)-1], nFrame)
+    Y.append(interpolate.splev(time, tck, der=0))
+
+###### visualisation
+
+plt.figure(1)
+for i in range(m.nbQ()):
+    plt.subplot(m.nbQ(), 2, 1+(2*i))
+    plt.plot(all_Q[i])
+    plt.title("états %i" %i)
+
+    plt.subplot(m.nbQ(), 2, 2+(2*i))
+    plt.plot(all_Qdot[i])
+    plt.title("états dérivés %i" %i)
+
+plt.figure(2)
+for j in range(nMuscle):
+    plt.subplot(nMuscle, 1, i+1)
+    plt.plot(all_U[i])
+    plt.title("contrôles %i" %j)
+
+plt.show()
+
+
+# Path to data
+nTags = m.nTags()
+mark_tp = np.ndarray((3,  nTags, nFrame))
+q_tp = np.ndarray((m.nbQ()))
+for t in range(nFrame):
+    for i in range(m.nbQ()):
+        q_tp[i] = Y[i][t]
+    Q = biorbd.s2mGenCoord(q_tp)
+    markers_tp = m.Tags(m, Q)
+
+    # Prepare markers
+    for j in range(nTags):
+        mark_tp[:, j, t] = markers_tp[j].get_array()
+all_marks = Markers3d(mark_tp)
+
+# Create a windows with a nice gray background
+vtkWindow = VtkWindow(background_color=(.5, .5, .5))
+
+# Add marker holders to the window
+vtkModelReal = VtkModel(vtkWindow, markers_color=(1, 0, 0), markers_size=.05, markers_opacity=1)
+
+# Animate all this
+i = 0
+while vtkWindow.is_active:
+    # Update markers
+    vtkModelReal.update_markers(all_marks.get_frame(i))
+
+    # Update window
+    vtkWindow.update_frame()
+    i = (i + 1) % all_marks.get_num_frames()
