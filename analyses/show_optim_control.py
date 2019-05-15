6d19f48e65cbbe1e01904af0604d82ff5e585fb3
pariterre <pariterre@hotmail.com>
2019-05-15 15:38:15 -0400

    Moved show_model and utils to analyses folder

--- analyses/show_optim_control.py
+++ analyses/show_optim_control.py
@@ -12,7 +12,7 @@ nb_phases = 1
 nb_frame_inter = 500
 
 # Load the biorbd model
-m = biorbd.s2mMusculoSkeletalModel(f"Modeles/ModeleAv2Muscles.bioMod")  # f"Modeles/Modele{model_name}.s2mMod")
+m = biorbd.s2mMusculoSkeletalModel(f"../optimal_control/Modeles/Modele{model_name}.bioMod")
 if fun_dyn == utils.dynamics_from_muscles:
     nb_controls = m.nbMuscleTotal()
 elif fun_dyn == utils.dynamics_from_joint_torque:
@@ -21,8 +21,8 @@ else:
     raise NotImplementedError("Dynamic not implemented yet")
 
 # Read values
-t, all_q, all_qdot = utils.read_acado_output_states(f"Resultats/States{model_name}.txt", m, nb_nodes, nb_phases)
-all_u = utils.read_acado_output_controls(f"Resultats/Controls{model_name}.txt", nb_nodes, nb_phases, nb_controls)
+t, all_q, all_qdot = utils.read_acado_output_states(f"../optimal_control/Results/States{model_name}.txt", m, nb_nodes, nb_phases)
+all_u = utils.read_acado_output_controls(f"../optimal_control/Results/Controls{model_name}.txt", nb_nodes, nb_phases, nb_controls)
 
 # Integrate
 t_integrate, q_integrate = utils.integrate_states_from_controls(m, t, all_q, all_qdot, all_u, fun_dyn,
@@ -51,7 +51,7 @@ plt.ion()  # Non blocking plt.show
 plt.show()
 
 # Animate the model
-b = BiorbdViz(loaded_model=m)
+b = BiorbdViz(loaded_model=m, show_muscles=False)
 frame = 0
 while b.vtk_window.is_active:
     b.set_q(q_interp[frame, :])
