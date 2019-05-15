3c60827a7665be1d7cce25f0919b48fa5bdfddb4
pariterre <pariterre@hotmail.com>
2019-05-15 14:17:22 -0400

    Converted show_model to the new API

--- optimal_control/show_model.py
+++ optimal_control/show_model.py
@@ -1,197 +1,59 @@
-"""
-Example script for animating a model
-"""
-import numpy as np
-import scipy.integrate as integrate
-import scipy.interpolate as interpolate
-
-from pyoviz.BiorbdViz import BiorbdViz
-import biorbd
-
 import matplotlib.pyplot as plt
+import biorbd
+from pyoviz.BiorbdViz import BiorbdViz
+
+import utils
+
+# Options
+model_name = "SansMuscle"
+fun_dyn = utils.dynamics_from_joint_torque
+nb_nodes = 30
+nb_phases = 1
+nb_frame_inter = 500
 
 # Load the biorbd model
+m = biorbd.s2mMusculoSkeletalModel(f"Modeles/ModeleAv2Muscles.bioMod")  # f"Modeles/Modele{model_name}.s2mMod")
+if fun_dyn == utils.dynamics_from_muscles:
+    nb_controls = m.nbMuscleTotal()
+elif fun_dyn == utils.dynamics_from_joint_torque:
+    nb_controls = m.nbTau()
+else:
+    raise NotImplementedError("Dynamic not implemented yet")
 
-m = biorbd.s2mMusculoSkeletalModel("Modeles/ModeleAv2Muscles.bioMod")
+# Read values
+t, all_q, all_qdot = utils.read_acado_output_states(f"Resultats/States{model_name}.txt", m, nb_nodes, nb_phases)
+all_u = utils.read_acado_output_controls(f"Resultats/Controls{model_name}.txt", nb_nodes, nb_phases, nb_controls)
 
-nFrame = 500            # number of frames
-nNoeuds = 30                # number of points
-nMuscle =m.nbMuscleTotal()
-nPhase = 1
-nPoints = (nPhase*nNoeuds)+1
-nQtotal = m.nbQ()+m.nbQdot()
+# Integrate
+t_integrate, q_integrate = utils.integrate_states_from_controls(m, t, all_q, all_qdot, all_u, fun_dyn,
+                                                                verbose=False, use_previous_as_init=True)
 
-##### State from the file
+# Interpolate
+t_interp, q_interp = utils.interpolate_integration(nb_frames=nb_frame_inter, t_int=t_integrate, y_int=q_integrate)
+qdot_interp = q_interp[:, m.nbQ():]
+q_interp = q_interp[:, :m.nbQ()]
 
-fichierS = open("Resultats/StatesAv2Muscles.txt", "r")
+# Show data
+plt.figure("States")
+for i in range(m.nbQ()):
+    plt.subplot(m.nbQ(), 3, 1+(3*i))
+    plt.plot(t_interp, q_interp[:, i])
+    plt.title("Q %i" %i)
 
-i = 0
-t = []                                      # initialization of the time
+    plt.subplot(m.nbQ(), 3, 2+(3*i))
+    plt.plot(t_interp, qdot_interp[:, i])
+    plt.title("Qdot %i" %i)
 
-all_Q = np.ones((m.nbQ(), nPoints))             # initialization of the states nbQ lines and nbP columns
-all_Qdot = np.ones((m.nbQdot(), nPoints))
+    plt.subplot(m.nbQ(), 3, 3+(3*i))
+    utils.plot_piecewise_constant(t, all_u[i, :])
+    plt.title("Control %i" %i)
+plt.ion()  # Non blocking plt.show
+plt.show()
 
-# initialization of the derived states
+# Animate the model
+b = BiorbdViz(loaded_model=m)
+frame = 0
+while b.vtk_window.is_active:
+    b.set_q(q_interp[frame, :])
+    frame = (frame+1) % nb_frame_inter
 
-# Nnoeuds first lines
-for l in range(nNoeuds):
-    ligne = fichierS.readline()
-    lin = ligne.split('\t')                 # separation of the line in element
-    lin[:1] = []                            # remove the first element ( [ )
-    lin[(nPhase*m.nbQ()) + (nPhase*m.nbQdot()) + 1:] = []     # remove the last ( ] )
-
-    t.append(float(lin[0]))                                                         # complete the time with the first column
-    for p in range(nPhase):
-        all_Q[:, i+p*nNoeuds] = [float(j) for j in lin[1+p*nQtotal:m.nbQ()+p*nQtotal+1]]                              # complete the states with the nQ next columns
-        all_Qdot[:, i+p*nNoeuds] = [float(k) for k in lin[m.nbQ()+1+p*nQtotal:nQtotal*(p+1)+1]]        # complete the states with the nQdot next columns
-
-    i += 1
-# Last line
-ligne = fichierS.readline()
-lin = ligne.split('\t')                 # separation of the line in element
-lin[:1] = []                            # remove the first element ( [ )
-lin[(nPhase*m.nbQ()) + (nPhase*m.nbQdot()) + 1:] = []     # remove the last ( ] )
-t.append(float(lin[0]))
-all_Q[:, -1] = [float(j) for j in lin[1+(nPhase-1)*nQtotal:m.nbQ()+(nPhase-1)*nQtotal+1]]                              # complete the states with the nQ next columns
-all_Qdot[:, -1] = [float(k) for k in lin[m.nbQ()+1+(nPhase-1)*nQtotal:nQtotal*nPhase+1]]
-
-
-fichierS.close()
-
-# Complete the time
-for p in range(1, nPhase):
-    for i in range(nNoeuds):
-        t.append(p+t[i+1])
-
-##### Controls from the file
-
-fichierU = open("Resultats/ControlsAv2Muscles.txt", "r")
-
-i = 0
-all_U = np.ones((nMuscle, nPoints))          # initialization of the controls
-
-for l in range(nNoeuds):
-    ligne = fichierU.readline()
-    lin = ligne.split('\t')
-    lin[:1] = []
-    lin[(nPhase*nMuscle) + 1:] = []
-
-    for p in range(nPhase):
-        all_U[:, i+p*nNoeuds] = [float(i) for i in lin[1+p*nMuscle:nMuscle*(p+1)+1]]
-
-    i += 1
-
-ligne = fichierU.readline()
-lin = ligne.split('\t')
-lin[:1] = []
-lin[(2*nMuscle) + 1:] = []
-
-all_U[:, -1] = [float(i) for i in lin[1+(nPhase-1)*nMuscle:nMuscle*nPhase+1]]
-
-fichierU.close()
-
-#### integration
-
-#dynamique avec activation des muscles
-def dyn(t_int, X):
-    states_actual = biorbd.VecS2mMuscleStateActual(nMuscle)
-    for i in range(len(states_actual)):
-        states_actual[i] = biorbd.s2mMuscleStateActual(0, u[i])
-
-    m.updateMuscles(m, X[:m.nbQ()], X[m.nbQ():], True)
-
-    Tau = biorbd.s2mMusculoSkeletalModel.muscularJointTorque(m, states_actual, X[:m.nbQ()], X[m.nbQ():])
-
-    QDDot = biorbd.s2mMusculoSkeletalModel.ForwardDynamics(m, X[:m.nbQ()], X[m.nbQ():], Tau).get_array()
-    rsh = np.ndarray(m.nbQ() + m.nbQdot())
-    for i in range(m.nbQ()):
-
-        rsh[i] = X[m.nbQ()+i]
-        rsh[i + m.nbQ()] = QDDot[i]
-
-    return rsh
-
-# #dynamique sans muscle
-# def dyn(t_int, X):
-#
-#     QDDot = biorbd.s2mMusculoSkeletalModel.ForwardDynamics(m, X[:m.nbQ()], X[m.nbQ():], u).get_array()
-#     rsh = np.ndarray(m.nbQ() + m.nbQdot())
-#     for i in range(m.nbQ()):
-#         rsh[i] = X[m.nbQ()+i]
-#         rsh[i + m.nbQ()] = QDDot[i]
-#
-#     return rsh
-
-# u=np.array([8.6843e-01])
-# X=np.array([1.7794e-01, -1.0142, 1.0794, -4.0377])
-# print(dyn(0, X))
-
-X = np.concatenate((all_Q[:, 0], all_Qdot[:, 0]))
-print("Solution initiale: ", X)
-u = all_U[:, 0]
-print("Control: ", u)
-print("Tstart: ", t[0], "\nTend: ", t[1])
-LI = integrate.solve_ivp(dyn, (t[0], t[1]), X, method='RK45', rtol=1e-6, atol=1e-8)
-T = LI.t.tolist()[0:len(LI.t.tolist())-1]
-I = LI.y[:, 0:len(T)]
-print("Fin integration reelle: ", LI.y[:, -1])
-print("Fin integration theorique(noeuds suivant): ", np.concatenate((all_Q[:, 1], all_Qdot[:, 1])))
-print("\n")
-
-for interval in range(1, len(t)-1):                 # integration between each point
-    u = all_U[:, interval]
-    X = np.concatenate((all_Q[:, interval], all_Qdot[:, interval]))
-    print("Solution initiale: ", X)
-    print("Control: ", u)
-    print("Tstart: ", t[interval], "\nTend: ", t[interval+1])
-    LI = integrate.solve_ivp(dyn, (t[interval], t[interval+1]), X, method='RK45', rtol=1e-6, atol=1e-8)
-    I = np.concatenate((I, LI.y[:, 0:len(LI.t.tolist())-1]), axis=1)
-    print("Fin integration reelle: ", LI.y[:, -1])
-    print("Fin integration theorique(noeuds suivant): ", np.concatenate((all_Q[:, interval+1], all_Qdot[:, interval+1])))
-    print("\n")
-    for i in range(len(LI.t)-1):
-        T.append(LI.t.tolist()[i])
-#
-# #### Interpolation
-# Q =np.ndarray((nFrame, m.nbQ()))
-# for q in range(m.nbQ()):
-#     tck = interpolate.splrep(T, I[q, :], s=0)
-#     time = np.linspace(0, T[len(T)-1], nFrame)
-#     Q[:, q] = interpolate.splev(time, tck, der=0)              # liste des positions à chaque temp: nQ listes de nFrames elements
-#
-# ###### visualisation
-#
-# plt.figure(1)
-# for i in range(m.nbQ()):
-#     plt.subplot(m.nbQ(), 2, 1+(2*i))
-#     plt.plot(all_Q[i])
-#     plt.title("états %i" %i)
-#
-#     plt.subplot(m.nbQ(), 2, 2+(2*i))
-#     plt.plot(all_Qdot[i])
-#     plt.title("états dérivés %i" %i)
-#
-# plt.figure(2)
-# for j in range(nMuscle):
-#     plt.subplot(nMuscle, 1, j+1)
-#     plt.plot(all_U[j])
-#     plt.title("contrôles %i" %j)
-#
-# plt.figure(3)
-# plt.subplot(1, 2, 1)
-# plt.plot(I[0])
-# plt.title("integration 0")
-# plt.subplot(1, 2, 2)
-# plt.plot(I[1])
-# plt.title("integration 1")
-#
-# plt.show()
-
-##### Animation
-#
-# b = BiorbdViz(loaded_model=m)
-#
-# i = 0
-# while b.vtk_window.is_active:
-#     b.set_q(Q[i, :])
-#     i = (i+1) % nFrame
---
+++ optimal_control/utils.py
@@ -0,0 +1,171 @@
+import numpy as np
+import biorbd
+from scipy import integrate, interpolate
+from matplotlib import pyplot as plt
+
+
+def read_acado_output_states(file_path, biorbd_model, nb_nodes, nb_phases):
+    # Get some values from the model
+    nb_dof_total = biorbd_model.nbQ() + biorbd_model.nbQdot()
+
+    # Create some aliases
+    nb_points = (nb_phases * nb_nodes) + 1
+
+    i = 0
+    t = np.ndarray((nb_points))  # initialization of the time
+
+    # initialization of the derived states
+    all_q = np.ndarray((biorbd_model.nbQ(), nb_points))  # initialization of the states nbQ lines and nbP columns
+    all_qdot = np.ndarray((biorbd_model.nbQdot(), nb_points))
+    with open(file_path, "r") as data:
+        # Nodes first lines
+        for l in range(nb_nodes):
+            line = data.readline()
+            lin = line.split('\t')  # separation of the line in element
+            lin[:1] = []  # remove the first element ( [ )
+            lin[(nb_phases * biorbd_model.nbQ()) + (nb_phases * biorbd_model.nbQdot()) + 1:] = []  # remove the last ( ] )
+
+            t[i] = float(lin[0])  # complete the time with the first column
+            for p in range(nb_phases):
+                all_q[:, i + p * nb_nodes] = [float(j) for j in lin[
+                                                               1 + p * nb_dof_total:biorbd_model.nbQ() + p * nb_dof_total + 1]]  # complete the states with the nQ next columns
+                all_qdot[:, i + p * nb_nodes] = [float(k) for k in lin[biorbd_model.nbQ() + 1 + p * nb_dof_total:nb_dof_total * (
+                            p + 1) + 1]]  # complete the states with the nQdot next columns
+            i += 1
+
+        # Last line
+        line = data.readline()
+        lin = line.split('\t')  # separation of the line in element
+        lin[:1] = []  # remove the first element ( [ )
+        lin[(nb_phases * biorbd_model.nbQ()) + (nb_phases * biorbd_model.nbQdot()) + 1:] = []  # remove the last ( ] )
+        t[-1] = float(lin[0])
+        all_q[:, -1] = [float(j) for j in lin[1 + (nb_phases - 1) * nb_dof_total:biorbd_model.nbQ() + (
+                    nb_phases - 1) * nb_dof_total + 1]]  # complete the states with the nQ next columns
+        all_qdot[:, -1] = [float(k) for k in lin[biorbd_model.nbQ() + 1 + (nb_phases - 1) * nb_dof_total:nb_dof_total * nb_phases + 1]]
+
+    # #TODO Complete the time when there is multiple phase
+    # for p in range(1, nb_phases):
+    #     for i in range(nb_nodes):
+    #         t.append(p + t[i + 1])
+    return t, all_q, all_qdot
+
+
+def read_acado_output_controls(file_path, nb_nodes, nb_phases, nb_controls):
+    i = 0
+
+    # Create some aliases
+    nb_points = (nb_phases * nb_nodes) + 1
+
+    all_u = np.ndarray((nb_controls, nb_points))
+    with open(file_path, "r") as fichier_u:
+
+        for l in range(nb_nodes):
+            line = fichier_u.readline()
+            lin = line.split('\t')
+            lin[:1] = []
+            lin[(nb_phases*nb_controls) + 1:] = []
+
+            for p in range(nb_phases):
+                all_u[:, i+p*nb_nodes] = [float(i) for i in lin[1+p*nb_controls:nb_controls*(p+1)+1]]
+
+            i += 1
+
+        line = fichier_u.readline()
+        lin = line.split('\t')
+        lin[:1] = []
+        lin[(2*nb_controls) + 1:] = []
+
+        all_u[:, -1] = [float(i) for i in lin[1+(nb_phases-1)*nb_controls:nb_controls*nb_phases+1]]
+    return all_u
+
+
+def dynamics_from_muscles(t_int, states, biorbd_model, u):
+    nb_q = biorbd_model.nbQ()
+    nb_qdot = biorbd_model.nbQdot()
+    nb_muscle = biorbd_model.nbMuscleTotal()
+
+    states_actual = biorbd.VecS2mMuscleStateActual(nb_muscle)
+    for i in range(len(states_actual)):
+        states_actual[i] = biorbd.s2mMuscleStateActual(0, u[i])
+
+    biorbd_model.updateMuscles(biorbd_model, states[:nb_q], states[nb_q:], True)
+    tau = biorbd.s2mMusculoSkeletalModel.muscularJointTorque(biorbd_model, states_actual, states[:nb_q], states[nb_q:])
+    qddot = biorbd.s2mMusculoSkeletalModel.ForwardDynamics(biorbd_model, states[:nb_q], states[nb_q:], tau).get_array()
+
+    rsh = np.ndarray(nb_q + nb_qdot)
+    for i in range(nb_q):
+        rsh[i] = states[nb_q+i]
+        rsh[i + nb_q] = qddot[i]
+
+    return rsh
+
+
+def dynamics_from_joint_torque(t_int, states, biorbd_model, u):
+    nb_q = biorbd_model.nbQ()
+    nb_qdot = biorbd_model.nbQdot()
+    qddot = biorbd.s2mMusculoSkeletalModel.ForwardDynamics(biorbd_model, states[:nb_q], states[nb_q:], u).get_array()
+    rsh = np.ndarray(nb_q + nb_qdot)
+    for i in range(nb_q):
+        rsh[i] = states[nb_q+i]
+        rsh[i + nb_q] = qddot[i]
+
+    return rsh
+
+import copy
+def integrate_states_from_controls(biorbd_model, t, all_q, all_qdot, all_u, dyn_fun, verbose=False,
+                                   use_previous_as_init=False):
+    all_t = np.ndarray(0)
+    integrated_state = np.ndarray((biorbd_model.nbQ() + biorbd_model.nbQdot(), 0))
+
+    q_init = np.concatenate((all_q[:, 0], all_qdot[:, 0]))
+    for interval in range(t.shape[0] - 1):  # integration between each point (but the last point)
+        u = all_u[:, interval]
+        integrated_tp = integrate.solve_ivp(
+            fun=lambda t, y: dyn_fun(t, y, biorbd_model, u),
+            t_span=(t[interval], t[interval + 1]), y0=q_init, method='RK45')
+
+        if use_previous_as_init:
+            q_init = integrated_tp.y[:, -1]
+        else:
+            q_init = np.concatenate((all_q[:, interval], all_qdot[:, interval]))
+        all_t = np.concatenate((all_t, integrated_tp.t[:-1]))
+        integrated_state = np.concatenate((integrated_state, integrated_tp.y[:, :-1]), axis=1)
+
+        if verbose:
+            print(f"Time: {t[interval]}")
+            print(f"Initial states: {q_init}")
+            print(f"Control: {u}")
+            print(f"Final states: {integrated_tp.y[:, -1]}")
+            print(f"Expected final states: {np.concatenate((all_q[:, interval + 1], all_qdot[:, interval + 1]))}")
+            print("")
+
+    return all_t, integrated_state
+
+
+def interpolate_integration(nb_frames, t_int, y_int):
+    nb_dof = y_int.shape[0]
+    q_interp = np.ndarray((nb_frames, nb_dof))
+    time_interp = np.linspace(0, t_int[-1], nb_frames)
+    for q in range(nb_dof):
+        tck = interpolate.splrep(t_int, y_int[q, :], s=0)
+        q_interp[:, q] = interpolate.splev(time_interp, tck, der=0)
+    return time_interp, q_interp
+
+
+def plot_piecewise_constant(t, data):
+    # Double the data
+    new_t = np.repeat(t, 2, axis=0)
+    if len(data.shape) == 1:
+        new_data = np.repeat(data, 2, axis=0)
+    else:
+        new_data = np.repeat(data, 2, axis=1)
+
+    # Realign
+    new_t = new_t[1:]
+    new_data = new_data[:-1]
+
+    plt.plot(new_t, new_data)
+
+
+def plot_piecewise_linear(t, data):
+    plt.plot(t, data)
