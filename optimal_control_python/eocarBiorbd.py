b3b2e30d03ceb5fda239675b6475c1bb2c1cb4e8
Théophile <theophile.gousselot@gmail.com>
2020-03-30 19:14:00 -0400

    removed unused constraint

--- optimal_control_python/eocarBiorbd.py
+++ optimal_control_python/eocarBiorbd.py
@@ -45,10 +45,7 @@ for i in range(biorbd_model.nbSegment()):
         ranges.append(biorbd_model.segment(i).ranges()[j])
 
 for i in range(biorbd_model.nbQ()):
-    if i == 0:
-        X_bounds.first_node_min.append(ranges[i].min())
-        X_bounds.first_node_max.append(ranges[i].max())
-    elif i == 3:
+    if i == 3:
         X_bounds.first_node_min.append(0)
         X_bounds.first_node_max.append(0)
     else:
@@ -56,10 +53,7 @@ for i in range(biorbd_model.nbQ()):
         X_bounds.first_node_max.append(ranges[i].max())
     X_bounds.min.append(ranges[i].min())
     X_bounds.max.append(ranges[i].max())
-    if i == 0:
-        X_bounds.last_node_min.append(ranges[i].min())
-        X_bounds.last_node_max.append(ranges[i].max())
-    elif i == 3:
+    if i == 3:
         X_bounds.last_node_min.append(1.57)
         X_bounds.last_node_max.append(1.57)
     else:
