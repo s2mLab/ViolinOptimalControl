9db5f9d5eec7553e6e934176ee8bc5bbab2a6516
Théophile <theophile.gousselot@gmail.com>
2020-05-25 18:13:25 -0400

    get_final data to give it to init x et u

--- optimal_control_python/load_file.py
+++ optimal_control_python/load_file.py
@@ -1,9 +1,10 @@
 import time
 import pickle
+import numpy as np
 
 from BiorbdViz import BiorbdViz
 
-file_path = 0
+file_path = "results/2020_5_25_2960sec_6c_m_i.bo"
 
 if not isinstance(file_path, str):
     t = time.localtime(time.time())
@@ -12,7 +13,21 @@ if not isinstance(file_path, str):
 with open(file_path, "rb") as file:
     data = pickle.load(file)
 
-data_interpolate, _ = data["data"]
-b = BiorbdViz("../models/BrasViolon.bioMod")
-b.load_movement(data_interpolate["q"].T)
-b.exec()
+data_interpolate, _ = data["get_data"]
+
+init = np.concatenate((data_interpolate["q"], data_interpolate["q_dot"]), 0)
+
+step = 80/49
+# init = np.delete(init, [int(step * k) for k in range(80 - 31)], 1)
+
+
+_, data_interpolate = data["get_data"]
+
+init = np.concatenate((data_interpolate["tau"], data_interpolate["muscles"]), 0)
+
+print(init)
+
+
+# b = BiorbdViz("../models/BrasViolon.bioMod")
+# b.load_movement(data_interpolate["q"].T)
+# b.exec()
