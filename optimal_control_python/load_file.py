a90a87dce237fffc67c89a46677ab770b79b502f
Théophile <theophile.gousselot@gmail.com>
2020-05-26 19:29:44 -0400

    Ready to use biorbdviz !

--- optimal_control_python/load_file.py
+++ optimal_control_python/load_file.py
@@ -4,30 +4,30 @@ import numpy as np
 
 from BiorbdViz import BiorbdViz
 
-file_path = "results/2020_5_25_2960sec_6c_m_i.bo"
+file_path = 0
 
 if not isinstance(file_path, str):
     t = time.localtime(time.time())
-    file_path = f"results/{t.tm_year}_{t.tm_mon}_{t.tm_mday}_up_and_down_numpy.bo"
+    file_path = f"results/{t.tm_year}_{t.tm_mon}_{t.tm_mday}_upDown_interpolate.bbo"
 
 with open(file_path, "rb") as file:
     data = pickle.load(file)
 
-data_interpolate, _ = data["get_data"]
+data_interpolate, _ = data["data"]
 
-init = np.concatenate((data_interpolate["q"], data_interpolate["q_dot"]), 0)
+# init = np.concatenate((data_interpolate["q"], data_interpolate["q_dot"]), 0)
+#
+# step = 80/49
+# # init = np.delete(init, [int(step * k) for k in range(80 - 31)], 1)
+#
+#
+# _, data_interpolate = data["get_data"]
+#
+# init = np.concatenate((data_interpolate["tau"], data_interpolate["muscles"]), 0)
+#
+# print(init)
 
-step = 80/49
-# init = np.delete(init, [int(step * k) for k in range(80 - 31)], 1)
 
-
-_, data_interpolate = data["get_data"]
-
-init = np.concatenate((data_interpolate["tau"], data_interpolate["muscles"]), 0)
-
-print(init)
-
-
-# b = BiorbdViz("../models/BrasViolon.bioMod")
-# b.load_movement(data_interpolate["q"].T)
-# b.exec()
+b = BiorbdViz("../models/BrasViolon.bioMod")
+b.load_movement(data_interpolate["q"].T)
+b.exec()
