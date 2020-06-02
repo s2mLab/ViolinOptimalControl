931421184a76eb4f249339bfac72b4f35b30f9ae
Théophile <theophile.gousselot@gmail.com>
2020-06-02 13:48:47 -0400

    add a file to read informations in a .bo

--- optimal_control_python/read_doc_bo.py
+++ optimal_control_python/read_doc_bo.py
@@ -2,7 +2,7 @@ import time
 
 from biorbd_optim import OptimalControlProgram
 
-file_path = "results/2020_5_26_upDown_6c_m_f_i.bo"
+file_path = "/home/theophile/Documents/programmation/ViolinOptimalControl/optimal_control_python/results/2020_6_2_upDown_6c_m_f_if.bo"
 
 if not isinstance(file_path, str):
     t = time.localtime(time.time())
