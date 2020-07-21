0822cf1f294a414cb0fbf3d6d8a27eb46ddbd206
Théophile <theophile.gousselot@gmail.com>
2020-07-21 19:53:01 -0400

    allow to call  read_doc_bo.py with parameters

--- optimal_control_python/read_doc_bo.py
+++ optimal_control_python/read_doc_bo.py
@@ -1,8 +1,15 @@
 import time
+import sys
 
 from biorbd_optim import OptimalControlProgram
 
-file_path = "/home/theophile/Documents/programmation/ViolinOptimalControl/optimal_control_python/results/2020_6_2_upDown_6c_m_f_if.bo"
+from up_and_down_bow import xia_model_dynamic, xia_model_configuration, xia_model_fibers, xia_initial_fatigue_at_zero
+
+
+file_path = "results/xia1/2020_7_17_upDown.bo"
+
+if len(sys.argv) > 1:
+    file_path = str(sys.argv[1])
 
 if not isinstance(file_path, str):
     t = time.localtime(time.time())
