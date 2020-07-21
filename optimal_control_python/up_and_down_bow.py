1f0d56d2bd5677dd8df00419fadee9d38fc8e004
Théophile <theophile.gousselot@gmail.com>
2020-07-21 19:52:33 -0400

    refactor (Biorbdoptim) and use pickle to load init values

--- optimal_control_python/up_and_down_bow.py
+++ optimal_control_python/up_and_down_bow.py
@@ -1,5 +1,5 @@
 import time
-
+import pickle
 import biorbd
 import numpy as np
 from casadi import MX, vertcat, if_else, lt, gt
@@ -7,14 +7,17 @@ from casadi import MX, vertcat, if_else, lt, gt
 from biorbd_optim import (
     Instant,
     InterpolationType,
+    InitialConditionsList,
     Axe,
+    BoundsList,
     OptimalControlProgram,
-    Dynamics,
+    DynamicsTypeList,
     Problem,
+    ConstraintList,
     CustomPlot,
     PlotType,
-    ProblemType,
     Objective,
+    ObjectiveList,
     Constraint,
     Bounds,
     QAndQDotBounds,
@@ -66,18 +69,17 @@ def xia_model_dynamic(states, controls, parameters, nlp):
                 ),
             )
             comp += 1
-        # todo lt "else" include equal ??
     restingdot = -command + Muscles.R * fatigued_fibers
     activatedot = command - Muscles.F * active_fibers
     fatiguedot = Muscles.F * active_fibers - Muscles.R * fatigued_fibers
 
     muscles_states = biorbd.VecBiorbdMuscleState(nlp["nbMuscle"])
-    for k in range(nlp["nbMuscles"]):
+    for k in range(nlp["nbMuscle"]):
         muscles_states[k].setActivation(active_fibers[k])
     # todo fix force max
 
     muscles_tau = nlp["model"].muscularJointTorque(muscles_states, q, qdot).to_mx()
-    # todo get muscle forces and multiplicate them by activate [k] and same as muscularJointTorque
+    # todo get muscle forces and multiply them by activate [k] and same as muscularJointTorque
     tau = muscles_tau + residual_tau
 
     dxdt = MX(nlp["nx"], nlp["ns"])
@@ -133,15 +135,23 @@ def xia_model_configuration(ocp, nlp):
         ylim=[0, 1],
     )
 
-    Problem.configure_forward_dyn_func(ocp, nlp, nlp["problem_type"]["dynamic"])
+    Problem.configure_forward_dyn_func(ocp, nlp, xia_model_dynamic)
 
 
-def xia_model_fibers_constraint(ocp, nlp, t, x, u, p):
-    offset = nlp["model"].nbQ() + nlp["model"].nbQdot()
-    nb_muscles = nlp["nbMuscles"]
+def xia_initial_fatigue_at_zero(ocp, nlp, t, x, u, p):
+    offset = nlp["nbQ"] + nlp["nbQdot"] + nlp["nbMuscle"]
     val = []
-    for k in range(nb_muscles):
-        val = vertcat(val, 1 - x[offset + k] - x[offset + k + nb_muscles] - x[offset + k + 2 * nb_muscles])
+    for k in range(nlp["nbMuscle"]):
+        val = vertcat(val, x[0][offset + k])
+    return val
+
+def xia_model_fibers(ocp, nlp, t, x, u, p):
+    offset = nlp["model"].nbQ() + nlp["model"].nbQdot()
+    val = []
+    for k in range(nlp["nbMuscle"]):
+        val = vertcat(
+            val, 1 - x[0][offset + k] - x[0][offset + k + nlp["nbMuscle"]] - x[0][offset + k + 2 * nlp["nbMuscle"]]
+        )
     return val
 
 
@@ -156,9 +166,9 @@ def prepare_ocp(biorbd_model_path="../models/BrasViolon.bioMod"):
     # --- Options --- #
     biorbd_model = biorbd.Model(biorbd_model_path)
     muscle_activated_init, muscle_fatigued_init, muscle_resting_init = 0, 0, 1
-    torque_min, torque_max, torque_init = -10, 10, 0
+    torque_min, torque_max, torque_init = -50, 50, 0
     muscle_states_ratio_min, muscle_states_ratio_max = 0, 1
-    number_shooting_points = 30
+    number_shooting_points = 25
     final_time = 0.5
 
     # --- String of the violin --- #
@@ -166,113 +176,137 @@ def prepare_ocp(biorbd_model_path="../models/BrasViolon.bioMod"):
     inital_bow_side = Bow("frog")
 
     # --- Objective --- #
-    objective_functions = (
-        {"type": Objective.Lagrange.MINIMIZE_MUSCLES_CONTROL, "weight": 10},
-        {"type": Objective.Lagrange.MINIMIZE_TORQUE, "weight": 1},
-        {"type": Objective.Lagrange.MINIMIZE_TORQUE, "controls_idx": [0, 1, 2, 3], "weight": 2000},
-    )
+    objective_functions = ObjectiveList()
+    objective_functions.add(Objective.Lagrange.MINIMIZE_MUSCLES_CONTROL, weight=1)
+    objective_functions.add(Objective.Lagrange.MINIMIZE_TORQUE, weight=1)
+    objective_functions.add(Objective.Lagrange.MINIMIZE_TORQUE, controls_idx=[0, 1, 2, 3], weight=100)
 
     # --- Dynamics --- #
-    problem_type = {"type": ProblemType.CUSTOM, "configure": xia_model_configuration, "dynamic": xia_model_dynamic}
+    dynamics = DynamicsTypeList()
+    dynamics.add(xia_model_configuration, dynamic_function=xia_model_dynamic)
 
     # --- Constraints --- #
-    constraints = (
-        {
-            "type": Constraint.ALIGN_MARKERS,
-            "instant": Instant.START,
-            "first_marker_idx": Bow.frog_marker,
-            "second_marker_idx": violon_string.bridge_marker,
-        },
-        {
-            "type": Constraint.ALIGN_MARKERS,
-            "instant": Instant.MID,
-            "first_marker_idx": Bow.tip_marker,
-            "second_marker_idx": violon_string.bridge_marker,
-        },
-        {
-            "type": Constraint.ALIGN_MARKERS,
-            "instant": Instant.END,
-            "first_marker_idx": Bow.frog_marker,
-            "second_marker_idx": violon_string.bridge_marker,
-        },
-        {
-            "type": Constraint.ALIGN_SEGMENT_WITH_CUSTOM_RT,
-            "instant": Instant.ALL,
-            "segment_idx": Bow.segment_idx,
-            "rt_idx": violon_string.rt_on_string,
-        },
-        {
-            "type": Constraint.ALIGN_MARKER_WITH_SEGMENT_AXIS,
-            "instant": Instant.ALL,
-            "marker_idx": violon_string.bridge_marker,
-            "segment_idx": Bow.segment_idx,
-            "axis": (Axe.Y),
-        },
-        {
-            "type": Constraint.ALIGN_MARKERS,
-            "instant": Instant.ALL,
-            "first_marker_idx": Bow.contact_marker,
-            "second_marker_idx": violon_string.bridge_marker,
-        },
-        # {"type": Constraint.CUSTOM, "function": xia_model_fibers_constraint, "instant": Instant.ALL,},
+    constraints = ConstraintList()
+    constraints.add(
+        Constraint.ALIGN_MARKERS,
+        instant=Instant.START,
+        min_bound=-0.001,
+        max_bound=0.001,
+        first_marker_idx=Bow.frog_marker,
+        second_marker_idx=violon_string.bridge_marker,
+    )
+    constraints.add(
+        Constraint.ALIGN_MARKERS,
+        instant=Instant.END,
+        min_bound=-0.001,
+        max_bound=0.001,
+        first_marker_idx=Bow.tip_marker,
+        second_marker_idx=violon_string.bridge_marker,
+    )
+    # constraints.add(
+    #     Constraint.ALIGN_MARKERS,
+    #     instant=Instant.END,
+    #     first_marker_idx=Bow.frog_marker,
+    #     second_marker_idx=violon_string.bridge_marker,
+    # )
+    constraints.add(
+        Constraint.ALIGN_SEGMENT_WITH_CUSTOM_RT,
+        instant=Instant.ALL,
+        min_bound=-0.001,
+        max_bound=0.001,
+        segment_idx=Bow.segment_idx,
+        rt_idx=violon_string.rt_on_string,
+    )
+    constraints.add(
+        Constraint.ALIGN_MARKER_WITH_SEGMENT_AXIS,
+        instant=Instant.ALL,
+        min_bound=-0.001,
+        max_bound=0.001,
+        marker_idx=violon_string.bridge_marker,
+        segment_idx=Bow.segment_idx,
+        axis=(Axe.Y),
+    )
+    constraints.add(
+        Constraint.ALIGN_MARKERS,
+        instant=Instant.ALL,
+        min_bound=-0.001,
+        max_bound=0.001,
+        first_marker_idx=Bow.contact_marker,
+        second_marker_idx=violon_string.bridge_marker,
         # TODO: add constraint about velocity in a marker of bow (start and end instant)
     )
+    constraints.add(xia_model_fibers, instant=Instant.ALL)
+    constraints.add(xia_initial_fatigue_at_zero, instant=Instant.START)
+
 
     # --- External forces --- #
     external_forces = [np.repeat(violon_string.external_force[:, np.newaxis], number_shooting_points, axis=1)]
 
     # --- Path constraints --- #
-    X_bounds = QAndQDotBounds(biorbd_model)
+    x_bounds = BoundsList()
+    x_bounds.add(QAndQDotBounds(biorbd_model))
 
-    X_bounds.min[biorbd_model.nbQ() :, [0, 2]] = 0
-    X_bounds.max[biorbd_model.nbQ() :, [0, 2]] = 0
+    x_bounds[0].min[biorbd_model.nbQ() :, [0, 2]] = 0
+    x_bounds[0].max[biorbd_model.nbQ() :, [0, 2]] = 0
     # todo compare with
 
     muscle_states_bounds = Bounds(
         [muscle_states_ratio_min] * biorbd_model.nbMuscleTotal() * 3,
         [muscle_states_ratio_max] * biorbd_model.nbMuscleTotal() * 3,
     )
-    X_bounds.concatenate(muscle_states_bounds)
+    x_bounds[0].concatenate(muscle_states_bounds)
 
-    U_bounds = Bounds(
-        [torque_min] * biorbd_model.nbGeneralizedTorque() + [muscle_states_ratio_min] * biorbd_model.nbMuscleTotal(),
-        [torque_max] * biorbd_model.nbGeneralizedTorque() + [muscle_states_ratio_max] * biorbd_model.nbMuscleTotal(),
+    u_bounds = BoundsList()
+    u_bounds.add(
+        [
+            [torque_min] * biorbd_model.nbGeneralizedTorque()
+            + [muscle_states_ratio_min] * biorbd_model.nbMuscleTotal(),
+            [torque_max] * biorbd_model.nbGeneralizedTorque()
+            + [muscle_states_ratio_max] * biorbd_model.nbMuscleTotal(),
+        ]
     )
 
     # --- Initial guess --- #
     optimal_initial_values = False
     if optimal_initial_values:
-        X_init = InitialConditions(violon_string.x_init, InterpolationType.EACH_FRAME)
-        U_init = InitialConditions(violon_string.u_init, InterpolationType.EACH_FRAME)
+        with open(f"utils/optimal_init_{number_shooting_points}_nodes.bio", "rb") as file:
+            dict = pickle.load(file)
+        x_init = InitialConditionsList()
+        x_init.add(dict["states"], interpolation=InterpolationType.EACH_FRAME)
+        u_init = InitialConditionsList()
+        u_init.add(dict["controls"], interpolation=InterpolationType.EACH_FRAME)
+
     else:
-        X_init = InitialConditions(
+        x_init = InitialConditionsList()
+        x_init.add(
             violon_string.initial_position()[inital_bow_side.side] + [0] * biorbd_model.nbQdot(),
-            InterpolationType.CONSTANT,
+            interpolation=InterpolationType.CONSTANT,
         )
-        U_init = InitialConditions(
+        u_init = InitialConditionsList()
+        u_init.add(
             [torque_init] * biorbd_model.nbGeneralizedTorque() + [muscle_activated_init] * biorbd_model.nbMuscleTotal(),
-            InterpolationType.CONSTANT,
+            interpolation=InterpolationType.CONSTANT,
         )
 
-    muscle_states_init = InitialConditions(
-        [muscle_activated_init] * biorbd_model.nbMuscleTotal()
-        + [muscle_fatigued_init] * biorbd_model.nbMuscleTotal()
-        + [muscle_resting_init] * biorbd_model.nbMuscleTotal(),
-        InterpolationType.CONSTANT,
-    )
-    X_init.concatenate(muscle_states_init)
+        muscle_states_init = InitialConditions(
+            [muscle_activated_init] * biorbd_model.nbMuscleTotal()
+            + [muscle_fatigued_init] * biorbd_model.nbMuscleTotal()
+            + [muscle_resting_init] * biorbd_model.nbMuscleTotal(),
+            interpolation=InterpolationType.CONSTANT,
+        )
+        x_init[0].concatenate(muscle_states_init)
 
     # ------------- #
 
     return OptimalControlProgram(
         biorbd_model,
-        problem_type,
+        dynamics,
         number_shooting_points,
         final_time,
-        X_init,
-        U_init,
-        X_bounds,
-        U_bounds,
+        x_init,
+        u_init,
+        x_bounds,
+        u_bounds,
         objective_functions,
         constraints,
         external_forces=external_forces,
@@ -288,12 +322,13 @@ if __name__ == "__main__":
     sol, sol_iterations = ocp.solve(
         show_online_optim=True,
         return_iterations=True,
-        options_ipopt={
+        solver_options={
             "tol": 1e-4,
-            "max_iter": 3000,
+            "max_iter": 500000,
             "ipopt.bound_push": 1e-10,
             "ipopt.bound_frac": 1e-10,
             "ipopt.hessian_approximation": "limited-memory",
+            "output_file": "output",
         },
     )
     toc = time.time() - tic
