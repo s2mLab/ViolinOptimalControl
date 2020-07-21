15fb9505322eab91ede1404d54da15a602f70b4b
Théophile <theophile.gousselot@gmail.com>
2020-07-21 19:48:39 -0400

    plot sol from a .bo

---
+++ optimal_control_python/load_file_graph.py
@@ -0,0 +1,21 @@
+import time
+import sys
+from biorbd_optim import OptimalControlProgram, ShowResult, Data
+from up_and_down_bow import xia_model_dynamic, xia_model_configuration, xia_model_fibers, xia_initial_fatigue_at_zero
+
+file_path = "results/xia/2020_7_20_upDown.bo"
+
+if len(sys.argv) > 1:
+    file_path = str(sys.argv[1])
+
+if not isinstance(file_path, str):
+    t = time.localtime(time.time())
+    file_path = f"results/{t.tm_year}_{t.tm_mon}_{t.tm_mday}_upDown.bo"
+
+ocp, sol = OptimalControlProgram.load(file_path)
+
+d = Data.get_data(ocp, sol)
+
+result = ShowResult(ocp, sol)
+result.graphs()
+
