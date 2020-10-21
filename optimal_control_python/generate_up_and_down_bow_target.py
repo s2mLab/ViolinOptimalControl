98711855030b08ac94039c33e2324fa578de1904
Ferreira <carla.ferreira@epfedu.fr>
2020-10-21 13:22:20 +0200

    modified the intergral in generate_up_and_down_bow_target

--- optimal_control_python/generate_up_and_down_bow_target.py
+++ optimal_control_python/generate_up_and_down_bow_target.py
@@ -4,6 +4,9 @@ from matplotlib import pyplot as plt
 import scipy.integrate as integrate
 
 
+min_bow = -0.07
+max_bow = -0.55
+
 def curve(x, t):
     period = 2 * np.pi / x[1]
     if isinstance(t, float):
@@ -24,34 +27,36 @@ def curve(x, t):
     return y
 
 def curve_integral(x, t):
-    integ = np.ndarray((t.shape[0]-1,))
+    integ = np.ndarray((t.shape[0],))
+    integ[0] = 0
     for i in range(t.shape[0]-1):
-        integ[i] = integrate.quad(lambda t: curve(x, t), 0, t[i+1])[0]
-    return integ
+        integ[i+1] = integrate.quad(lambda t: curve(x, t), 0, t[i+1])[0]
+    return integ + min_bow
 
-def generate_up_and_down_bow_target(ns, bow_speed=10, bow_acceleration=0.5):
+def generate_up_and_down_bow_target(n_points, bow_speed=10, bow_acceleration=0.5):
 
 
     def objective_function(x, t):
         y = curve(x, t)
         integ = curve_integral(x,t)
         moy = (integ[int(n_points/2)-2] + integ[int(n_points/2)+2])/2
-        return np.array((bow_acceleration-x[0], bow_speed-x[1], y[-1], y[0], (moy-0.48)*1000))
+        return np.array((bow_acceleration-x[0], bow_speed-x[1], y[-1], y[0], (moy-(max_bow - min_bow))*1000))
 
-    n_points = 200
     t = np.linspace(0, 2, n_points)
     x_opt = optimize.least_squares(lambda x: objective_function(x, t), x0=np.array((1, 8))) # x[0] = amplitude et x[1]= 2 pi/ period
-    return t, x_opt.x
+    return x_opt.x
 
 
 if __name__ == "__main__":
-    t, x = generate_up_and_down_bow_target(0)
+    x = generate_up_and_down_bow_target(200)
     n_points = 200
     t = np.linspace(0, 2, n_points)
+    plt.title("Speed and position of the virtual contact on the bow during the up and down movement")
     plt.plot(t, curve(x, t))
-    plt.plot(t[:-1], curve_integral(x, t), color="red")
+    plt.plot(t, curve_integral(x, t), color="red")
+    # plt.plot(t[:-1], curve_integral(x, t), color="red")
     n_points = 50
     t = np.linspace(0, 2, n_points)
     plt.plot(t, curve(x, t), 'k.')
-    plt.plot(t[:-1], curve_integral(x, t), '.m')
+    # plt.plot(t[:-1], curve_integral(x, t), '.m')
     plt.show()
--- optimal_control_python/up_and_down_NMPC.py
+++ optimal_control_python/up_and_down_NMPC.py
@@ -1,6 +1,9 @@
 import biorbd
 import numpy as np
 import matplotlib
+from optimal_control_python.generate_up_and_down_bow_target import generate_up_and_down_bow_target
+from optimal_control_python.generate_up_and_down_bow_target import curve_integral
+
 
 from bioptim import (
     OptimalControlProgram,
@@ -20,7 +23,7 @@ from bioptim import (
 from optimal_control_python.utils import Bow, Violin
 
 
-def prepare_ocp(biorbd_model_path, number_shooting_points, final_time, x_init, u_init, x0,):
+def prepare_ocp(biorbd_model_path, number_shooting_points, final_time, x_init, u_init, x0, ):
     biorbd_model = biorbd.Model(biorbd_model_path)
     n_tau = biorbd_model.nbGeneralizedTorque()
     tau_min, tau_max, tau_init = -100, 100, 0
@@ -45,6 +48,22 @@ def prepare_ocp(biorbd_model_path, number_shooting_points, final_time, x_init, u
     u_bounds = BoundsOption([[tau_min] * n_tau, [tau_max] * n_tau])
     u_init = InitialGuessOption(u_init, interpolation=InterpolationType.EACH_FRAME)
 
+    new_constraints = ConstraintList()
+    for j in range(1, 5):
+        new_constraints.add(Constraint.ALIGN_MARKERS,
+                            instant=j,
+                            min_bound=0,
+                            max_bound=0,
+                            first_marker_idx=Bow.contact_marker,
+                            second_marker_idx=violin.bridge_marker)
+    for j in range(5, nb_shooting_pts_window + 1):
+        new_constraints.add(Constraint.ALIGN_MARKERS,
+                            instant=j,
+                            # min_bound=-1, #-10**(j-14) donne 25 itérations
+                            # max_bound=1, # (j-4)/10 donne 21 itérations
+                            first_marker_idx=Bow.contact_marker,
+                            second_marker_idx=violin.bridge_marker)
+
     return OptimalControlProgram(
         biorbd_model,
         dynamics,
@@ -55,23 +74,28 @@ def prepare_ocp(biorbd_model_path, number_shooting_points, final_time, x_init, u
         x_bounds,
         u_bounds,
         objective_functions=objective_functions,
+        constraints=new_constraints,
     ), x_bounds
 
 
 
-
-
-def warm_start_mhe(data_sol_prev):
+def warm_start_mhe(sol):
+    data_sol_prev = Data.get_data(ocp, sol, concatenate=False)
     q = data_sol_prev[0]["q"]
     dq = data_sol_prev[0]["q_dot"]
     u = data_sol_prev[1]["tau"]
     x = np.vstack([q, dq])
     X_out = x[:, 0]
     U_out = u[:, 0]
-    x_init = x  # np.hstack((x[:, 1:], np.tile(x[:, [-1]], 1)))  # discard oldest estimate of the window, duplicates youngest
+    x_init = x
     u_init = u[:, :-1]  # [:, 1:]  # discard oldest estimate of the window
     return x_init, u_init, X_out, U_out
 
+    #
+    # X0 = np.hstack((x[:, 1:], np.tile(x[:, [-1]], 1)))  # discard oldest estimate of the window, duplicates youngest
+    # U0 = u[:, 1:]  # discard oldest estimate of the window
+    # X_out = x[:, 0]
+    # return X0, U0, X_out
 
 if __name__ == "__main__":
     biorbd_model_path = "/home/carla/Documents/Programmation/ViolinOptimalControl/models/BrasViolon.bioMod"
@@ -81,7 +105,7 @@ if __name__ == "__main__":
     n_tau = biorbd_model.nbGeneralizedTorque()
     n_muscles = biorbd_model.nbMuscles()
     final_time = 1/3  # duration of the simulation
-    nb_shooting_pts_window = 15  # size of MHE window
+    nb_shooting_pts_window = 15  # size of NMPC window
     ns_tot = nb_shooting_pts_window*2 # size of the entire optimization
     time_tot=2/3
 
@@ -100,7 +124,47 @@ if __name__ == "__main__":
     X_est = np.zeros((n_q * 2, nb_shooting_pts_window+1))
     U_est = np.zeros((n_tau * 2, nb_shooting_pts_window))
 
+    bow_target_param = generate_up_and_down_bow_target(200)
+    # position initiale de l'ocp
+
+    ocp, x_bounds = prepare_ocp(
+        biorbd_model_path=biorbd_model_path,
+        number_shooting_points=nb_shooting_pts_window,
+        final_time=final_time,
+        x_init=x_init,
+        u_init=u_init,
+        x0=x0,
+        # q_ref=q_ref,
+    )
+
     for i in range(ns_tot):
+
+        n_points = 200
+        final_time
+        t = np.linspace(0, final_time, nb_shooting_pts_window + 1)
+        q_target = np.ndarray((n_q, nb_shooting_pts_window + 1))
+        q_target[bow.hair_idx, :] = curve_integral(bow_target_param, t)
+
+        new_objectives = ObjectiveList()
+
+        new_objectives.add(Objective.Lagrange.MINIMIZE_TORQUE, instant=Instant.ALL, state_idx=bow.hair_idx,
+                           weight=1,
+                           idx=3) # permet de réduire le nombre d'itérations avant la convergence
+        new_objectives.add(
+            Objective.Lagrange.TRACK_STATE, instant=Instant.ALL, weight=100, target=q_target, states_idx=bow.hair_idx, idx=4
+        )
+        # new_objectives.add(Objective.Lagrange.MINIMIZE_TORQUE_DERIVATIVE, instant=Instant.ALL, state_idx=bow.hair_idx,
+        #                    # weight=1,
+        #                    idx=4)  # rajoute des itérations et ne semble riuen changer au mouvement...
+        ocp.update_objectives(new_objectives)
+
+        sol = ocp.solve(
+            show_online_optim=False,
+            solver_options={"max_iter": 1000, "hessian_approximation": "exact", "bound_push": 10**(-10), "bound_frac": 10**(-10)}  #, "bound_push": 10**(-10), "bound_frac": 10**(-10)}
+        )
+
+        data_sol_prev = Data.get_data(ocp, sol, concatenate=False)
+        x_init, u_init, X_out, U_out = warm_start_mhe(sol)
 
         ocp, x_bounds = prepare_ocp(
             biorbd_model_path=biorbd_model_path,
@@ -111,42 +175,12 @@ if __name__ == "__main__":
             x0=x0,
         )
 
-        bow_target[bow.hair_idx, -1] = -0.23  # + i/100
-        new_objectives = ObjectiveList()
-
-        new_objectives.add(Objective.Mayer.TRACK_STATE, instant=Instant.END, states_idx=bow.hair_idx, target=bow_target,
-                           weight=1000, idx=2)
-        new_objectives.add(Objective.Lagrange.MINIMIZE_TORQUE, instant=Instant.ALL, state_idx=bow.hair_idx,
-                           weight=1,
-                           idx=3) # permet de réduire le nombre d'itérations avant la convergence
-        new_objectives.add(Objective.Lagrange.MINIMIZE_TORQUE_DERIVATIVE, instant=Instant.ALL, state_idx=bow.hair_idx,
-                           # weight=1,
-                           idx=4)  # rajoute des itérations et ne semble riuen changer au mouvement...
-        ocp.update_objectives(new_objectives)
-
-
-        new_constraints = ConstraintList()
-        for j in range(1, 5):
-            new_constraints.add(Constraint.ALIGN_MARKERS,
-                                instant=j,
-                                min_bound=0,
-                                max_bound=0,
-                                first_marker_idx=Bow.contact_marker,
-                                second_marker_idx=violin.bridge_marker)
-        for j in range(5, nb_shooting_pts_window+1):
-            new_constraints.add(Constraint.ALIGN_MARKERS,
-                                instant=j,
-                                # min_bound=-1, #-10**(j-14) donne 25 itérations
-                                # max_bound=1, # (j-4)/10 donne 21 itérations
-                                first_marker_idx=Bow.contact_marker,
-                                second_marker_idx=violin.bridge_marker)
-
-        ocp.update_constraints(new_constraints)
 
         sol = ocp.solve(
             show_online_optim=False,
-            solver_options={"max_iter": 0, "hessian_approximation": "exact"}  #, "bound_push": 10**(-10), "bound_frac": 10**(-10)}
+            solver_options={"max_iter": 0, "hessian_approximation": "exact", "bound_push": 10**(-10), "bound_frac": 10**(-10)}  #, "bound_push": 10**(-10), "bound_frac": 10**(-10)}
         )
+
 
         matplotlib.pyplot.figure(1)
 
@@ -157,6 +191,7 @@ if __name__ == "__main__":
             matplotlib.pyplot.subplot(2, 5, int(idx+1))
 
             matplotlib.pyplot.plot(data_sol[0]["q"][idx], color="blue") # degré de liberté idx à tous les noeuds
+            matplotlib.pyplot.plot(data_sol_prev[0]["q"][idx], color="yellow")
             matplotlib.pyplot.title(f"dof {idx}")
             matplotlib.pyplot.plot(x_init[idx, :], color="red") # degré de liberté idx à tous les noeuds
             matplotlib.pyplot.plot(x_bounds.min[idx, :], color="green")
@@ -169,6 +204,7 @@ if __name__ == "__main__":
             matplotlib.pyplot.subplot(2, 5, int(idx+1))
             matplotlib.pyplot.title(f"dof {idx}")
             matplotlib.pyplot.plot(data_sol[0]["q_dot"][idx], color="blue") # degré de liberté idx à tous les noeuds
+            matplotlib.pyplot.plot(data_sol_prev[0]["q_dot"][idx], color="yellow")
             matplotlib.pyplot.plot(x_init[idx+n_q, :], color="red") # degré de liberté idx à tous les noeuds
             matplotlib.pyplot.plot(x_bounds.min[idx+n_q, :], color="green")
             matplotlib.pyplot.plot(x_bounds.max[idx+n_q, :], color="green")
@@ -184,10 +220,6 @@ if __name__ == "__main__":
             matplotlib.pyplot.plot(u_init[idx, :], color="red") # degré de liberté idx à tous les noeuds
         matplotlib.pyplot.show()
 
-
-
-
-        # matplotlib.pyplot.sumplot()
 
 
         print(f"NUMERO DE LA FENETRE : {i}")
