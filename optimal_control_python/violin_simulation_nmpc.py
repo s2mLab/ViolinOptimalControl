8c979fdb5f6e4063da6be92e9105569e3337ceb8
Pariterre <pariterre@hotmail.com>
2021-05-04 16:22:32 -0400

    "Working" version of ocp and mhe

--- optimal_control_python/violin_simulation_nmpc.py
+++ optimal_control_python/violin_simulation_nmpc.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     bow = Bow(model_name)
 
     # --- Solve the program --- #
-    window = 30
+    window = 7
     full_cycle = 30
     cycle_time = 1
     n_cycles = 3
@@ -31,15 +31,14 @@ if __name__ == "__main__":
     bow_trajectory = BowTrajectory(lim, full_cycle + 1)
 
     def nmpc_update_function(ocp, t, sol):
-        print(t)
         target_time_index = [i % full_cycle for i in range(t, t + window + 1)]
         ocp_violin.set_bow_target_objective(bow_trajectory.target[:, target_time_index])
-        return t < n_cycles * full_cycle - window
+        return t < n_cycles * full_cycle
 
     tic = time()
-    full_sol = ocp_violin.solve(nmpc_update_function)
-    print(f"Running time: {time() - tic}")
+    sol = ocp_violin.solve(nmpc_update_function)
+    print(f"Running time: {time() - tic} seconds")
 
-    # sol.print()
-    full_sol.graphs()
-    full_sol.animate(show_meshes=False)
+    sol.print()
+    sol.graphs()
+    sol.animate(show_meshes=False)
---
+++ optimal_control_python/violin_simulation_ocp.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+from violin_ocp import Violin, ViolinString, ViolinOcp, Bow, BowTrajectory, BowPosition
+from bioptim import Solver
+
+
+if __name__ == "__main__":
+    model_name = "WuViolin"
+    violin = Violin(model_name, ViolinString.E)
+    bow = Bow(model_name)
+
+    # --- Solve the program --- #
+    n_shoot_per_cycle = 30
+    cycle_time = 1
+    n_cycles = 3
+    solver = Solver.IPOPT
+    ocp = ViolinOcp(
+        model_path=f"../models/{model_name}.bioMod",
+        violin=violin,
+        bow=bow,
+        n_cycles=3,
+        bow_starting=BowPosition.TIP,
+        init_file=None,
+        use_muscles=False,
+        time_per_cycle=cycle_time,
+        n_shooting_per_cycle=n_shoot_per_cycle,
+        solver=solver,
+    )
+    # ocp, sol = ViolinOcp.load("results/5_cycles_34_muscles/2021_3_12.bo")
+
+    lim = bow.hair_limits if ocp.bow_starting == BowPosition.FROG else [bow.hair_limits[1], bow.hair_limits[0]]
+    bow_trajectory = BowTrajectory(lim, ocp.n_shooting_per_cycle + 1)
+    bow_target = np.tile(bow_trajectory.target[:, :-1], ocp.n_cycles)
+    bow_target = np.concatenate((bow_target, bow_trajectory.target[:, -1][:, np.newaxis]), axis=1)
+    ocp.set_bow_target_objective(bow_target)
+
+    sol = ocp.solve(
+        show_online_optim=True,
+        solver_options={"max_iter": 1000, "hessian_approximation": "exact", "linear_solver": "ma57"},
+    )
+    ocp.save(sol)
+    ocp.save(sol, stand_alone=True)
+
+    sol.print()
+    sol.animate(show_meshes=False)
